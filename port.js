326c7ec1f31b1d9c9f154095da4236b89ba1d679
sklugherz <sklugherz@gmail.com>
2025-02-19 12:06:18 -0800

    initial aobut layout

---
+++ port.js
@@ -0,0 +1,13 @@
+const bioToggle = document.getElementById('bio-toggle');
+const bio1 = document.getElementById('sde');
+const bio2 = document.getElementById('csp');
+
+bioToggle.addEventListener('change', function() {
+  if (this.checked) {
+    bio1.style.display = 'none';
+    bio2.style.display = 'block';
+  } else {
+    bio1.style.display = 'block';
+    bio2.style.display = 'none';
+  }
+});
