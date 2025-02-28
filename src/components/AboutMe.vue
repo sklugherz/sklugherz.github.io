b2c3ffde8be4cf47e37d78bce877f3e29af33525
Stephen Klugherz <111817710+sklugherz@users.noreply.github.com>
2025-02-28 11:11:36 -0800

    Update AboutMe.vue
    spellinge rrors

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -8,15 +8,15 @@
 	<div class="about">
 		<h3>Software Engineer &amp Cybersecurity Professional</h3>
 		<p>
-			I'm a software developer based in the US. I enjoy finding cool open-source projects and partcipating in CTFs. 
-			I'm interested in cybersecuirty and game development.
+			I'm a software developer based in the US. I enjoy finding cool open-source projects and participating in CTFs. 
+			I'm interested in cybersecurity and game development.
 			<br>
 			<br>
 			I hold two (ISC)² certifications -
 				<a href="https://www.credly.com/badges/5c8c8f44-3039-45ec-a7f2-35b6569b566a" target="_blank">SSCP</a> 
 				and 
 				<a href="https://www.credly.com/badges/2d03bc40-9f83-4347-a505-f780d27d1e41/public_url"target="_blank">CC</a>
-				- demonstrating cybersecurity expertise. 
+				- demonstrating my foundation in cybersecurity expertise. 
 			Through independent study and hands-on labs, I've gained experience with SIEM tools, 
 			network security, and security protocols.
 		</p>
