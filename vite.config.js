c38a9a7002aabd4406d8ff2211f540b48e1f3100
sklugherz <sklugherz@gmail.com>
2025-02-27 20:46:14 -0800

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: process.env.NODE_ENV === 'production'
+    ? '/dist/'
+    : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
