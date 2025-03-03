5fac69bcf317961123fc4b6823a033cba94e5b42
sklugherz <sklugherz@gmail.com>
2025-03-03 11:39:59 -0800

    s

---
+++ src/templates/uses-list.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+    <p>Inspired by: <a href="https://uses.tech">Uses/</a></p>
+    <h3>Hardware</h3>
+    <h4>Modified Lenovo Ideapad Laptop</h4>
+    <ul>
+        <li>
+            <p>OS: Kali Linux</p>
+        </li>
+        <li>
+            <p>Processor: Intel I3</p>
+        </li>
+        <li>
+            <p>GPU: Integrated</p>
+        </li>
+        <li>
+            <p>Upgraded to 32GB ram</p>
+        </li>
+    </ul>
+   
+        
+</template>
+
+<style scoped>
+
+ul {
+  margin-left: auto;
+  margin-right: auto;
+  width: 90%;
+}
+
+a {
+  border-bottom: dashed 1px white;
+}
+
+a:hover {
+  border-bottom: dashed 1px #f67634;
+}
+
+</style>
