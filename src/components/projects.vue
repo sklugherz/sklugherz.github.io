5c2cbe5097b03a1474e0726a102f864d4f3ab461
sklugherz <sklugherz@gmail.com>
2025-03-03 21:04:08 -0800

    everything changes

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,19 +1,25 @@
 <script setup>
 
-const projects = ref({id:'1',
-                    title: 'P1', 
-                    desc: 'p1 is awesome', 
-                    img: '../assets/imgs/picoctf.png'})
+import {ref} from 'vue';
 
+const projects = ref([{title: 'project 1', 
+                    desc: 'hello world',}, {title: 'project 2', 
+                    desc: 'hello world',}]);
+
+const isSelected = ref(false);
+
+function toggleSelected() {
+    isSelected.value = !isSelected.value;
+}
 
 </script>
 
 <template>
     <div class="main">
         <div class="aside">
-            <ul>>
-                <li v-for="project in projects" :key="project.id">
-                    {{ project.title }}
+            <ul>
+                <li v-for="project in projects" @click="toggleSelected()" :class="{ selected: isSelected }">
+                    <p>{{ project.title }}</p><span>&nbsp;>></span>
                 </li>
             </ul>
         </div>
@@ -46,12 +52,28 @@ li {
     border: 1px solid white;
     border-top: hidden;
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
 }
 
 li:first-child {
     border-top: 1px solid white;
+}
+
+span { 
+    display: none;
+}
+
+.selected span {
+    display: block;
+}
+
+.selected {
+    border-right: hidden;
+}
+
+.selected li {
+    padding-left: 5px;
 }
 
 .aside {
