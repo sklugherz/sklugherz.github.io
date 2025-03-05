e1aa57e50f495b4e5b35271ba0facca2b457559e
sklugherz <sklugherz@gmail.com>
2025-03-05 15:16:31 -0800

    last

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -6,8 +6,8 @@ const projects = [
     {title: 'ReqScan', img: '', keywords: 'Python, NVD, API, CLI', desc: 'A simple CLI tool; Checks required project dependencies against the National Vulernability Database. Generates a report of vulnerabilty findings.'}, 
     {title: 'KingsRoad', img:'', keywords: 'Python, PyGame, SDL2', desc: 'A 2D arcade style fighter game with a medevial theme; Pits two players against eachother with chosen avatars from medieval times. Base for future 2D scroller D&D style game.'},
     {title: 'GeneticAlgo', img:'', keywords: 'Python, Jupyter, Machine Learning, Research', desc: 'Collaborative ML research project that attempted to link genetic algorithm optimization with stock trading.'},
-    {title: 'Censor', img:'', keywords: 'C++, CLI', desc: 'CLI tool for censoring text files or a stream of text given a custom set of prohibited words.'},
-    {title: 'ProgramScheduler', img:'', keywords: 'C++', desc: 'A look into what the back-end logic of a TV menu might look like.'},
+    {title: 'Censorer', img:'', keywords: 'C++, CLI', desc: 'CLI tool for censoring text files or a stream of text given a custom set of prohibited words.'},
+    {title: 'TV Menu', img:'', keywords: 'C++', desc: 'A look into what the back-end logic of a TV menu might look like.'},
 
 
     ];
