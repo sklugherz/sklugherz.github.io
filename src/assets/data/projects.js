146024b5c45cb2edb117ca71446c918cd86d2465
sklugherz <sklugherz@gmail.com>
2025-03-05 15:14:34 -0800

    finished transfering porjects from archives

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,44 +1,16 @@
 
 
-// import from '../imgs/porjects/'
+// import x from '../imgs/porjects/'
 
 const projects = [
-    {title: 'ReqScan', img: '', keywords: 'Python, NVD, API', desc: 'A simple CLI tool; Checks required project dependencies against the National Vulernability Database. Generates a report of vulnerabilty findings.'}, 
+    {title: 'ReqScan', img: '', keywords: 'Python, NVD, API, CLI', desc: 'A simple CLI tool; Checks required project dependencies against the National Vulernability Database. Generates a report of vulnerabilty findings.'}, 
     {title: 'KingsRoad', img:'', keywords: 'Python, PyGame, SDL2', desc: 'A 2D arcade style fighter game with a medevial theme; Pits two players against eachother with chosen avatars from medieval times. Base for future 2D scroller D&D style game.'},
-    {title: 'GeneticAlgo', img:'', keywords: '', desc: ''},
+    {title: 'GeneticAlgo', img:'', keywords: 'Python, Jupyter, Machine Learning, Research', desc: 'Collaborative ML research project that attempted to link genetic algorithm optimization with stock trading.'},
+    {title: 'Censor', img:'', keywords: 'C++, CLI', desc: 'CLI tool for censoring text files or a stream of text given a custom set of prohibited words.'},
+    {title: 'ProgramScheduler', img:'', keywords: 'C++', desc: 'A look into what the back-end logic of a TV menu might look like.'},
+
 
     ];
     //{title: '', img:'', keywords: '', desc: ''},
 
 export default projects;
-
-
-/*
-
-<tr>
-<td>Dependency Scanner</td>
-<td>CLI tool; Checks project dependencies against National Vulernability Database.</td>
-<td class="cc">In Development</td>
-</tr>
-<tr>
-<td>Medevial Street Fighter</td>
-<td>D arcade style fighter game with a medevial themes; Base for future project.</td>
-<td class="cc">In Development</td>
-</tr>
-<tr>
-<td>Genetic-Algorithm TSP</td>
-<td>Collaborative ML research project that attempts to link G.A. with stock trading.</td>
-<td class="yellow">Archived</td>
-</tr>
-<tr>
-<td>File Censor</td>
-<td>CLI tool for censoring text files given a custom set of prohibited words.</td>
-<td class="sscp">Maintenance</td>
-</tr>
-<tr>
-<td>Program Scheduler</td>
-<td>A look into what the back-end logic of a TV menu might look like.</td>
-<td class="sscp">Maintenance</td>
-</tr>
-
-*/
