de406780324e2930af5546aba65823a69f8b589e
sklugherz <sklugherz@gmail.com>
2025-03-10 14:13:30 -0700

    remove bad proejct

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -9,7 +9,6 @@ const projects = [
     {title: 'ReqScanner', github: 'https://github.com/sklugherz/Dependency-Scanner', img: '', keywords: 'Python, NVD, API, CLI', desc: 'A simple CLI tool; Checks required project dependencies against the National Vulernability Database. Generates a report of vulnerabilty findings.'}, 
     {title: 'KingsRoad', github: 'https://github.com/sklugherz/MedievalStreetFighter', img: KR, keywords: 'Python, PyGame, SDL2', desc: 'A 2D arcade style fighter game with a medevial theme; Pits two players against eachother with chosen avatars from medieval times. Base for future 2D scroller D&D style game.'},
     {title: 'GeneticAlgo', github: 'https://github.com/sklugherz/CS345-Genetic-Algorithm/blob/sk-reporttext/quad.ipynb', img: GA, keywords: 'Python, Jupyter, Machine Learning, Research', desc: 'Collaborative ML research project that attempted to link genetic algorithm optimization with stock trading.'},
-    {title: 'Censorer', github: 'https://github.com/sklugherz/File-Censor', img:'', keywords: 'C++, CLI', desc: 'CLI tool for censoring text files or a stream of text given a custom set of prohibited words.'},
     {title: 'TV Menu', github: 'https://github.com/sklugherz/Program-Scheduler', img:'', keywords: 'C++', desc: 'A look into what the back-end logic of a TV menu might look like.'},
 
 
