c00e241e7509e035ae894ddd2f49c6c5fe1c0bbd
sklugherz <sklugherz@gmail.com>
2025-03-25 16:36:36 -0700

    add page, fix typo

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -5,8 +5,8 @@ import PS from '../imgs/projects/ps.png'
 // import x from '../imgs/projects/'
 
 const projects = [
-    {title: 'ReqScanner', github: 'https://github.com/sklugherz/Dependency-Scanner', img: '', keywords: 'Python, NVD, API, CLI', desc: 'A simple CLI tool; Checks required project dependencies against the National Vulernability Database. Generates a report of vulnerabilty findings.'}, 
-    {title: 'KingsRoad', github: 'https://github.com/sklugherz/MedievalStreetFighter', img: KR, keywords: 'Python, PyGame, SDL2', desc: 'A 2D arcade style fighter game with a medevial theme; Pits two players against eachother with chosen avatars from medieval times. Base for future 2D scroller D&D style game.'},
+    {title: 'ReqScanner', github: 'https://github.com/sklugherz/Dependency-Scanner', img: '', keywords: 'Python, NVD, API, CLI', desc: 'A simple CLI tool; Checks required project dependencies against the National Vulnernability Database. Generates a report of vulnerabilty findings.'}, 
+    {title: 'KingsRoad', github: 'https://github.com/sklugherz/MedievalStreetFighter', img: KR, keywords: 'Python, PyGame, SDL2', desc: 'A 2D arcade style fighter game with a medevial theme; Pits two players against one another with chosen avatars from medieval times. Base for future 2D scroller D&D style game.'},
     {title: 'GeneticAlgo', github: 'https://github.com/sklugherz/CS345-Genetic-Algorithm/blob/sk-reporttext/quad.ipynb', img: GA, keywords: 'Python, Jupyter, Machine Learning, Research', desc: 'Collaborative ML research project that attempted to link genetic algorithm optimization with stock trading.'},
     {title: 'TV Menu', github: 'https://github.com/sklugherz/Program-Scheduler', img: PS, keywords: 'C++', desc: 'A look into what the back-end logic of a TV menu might look like.'},
 
