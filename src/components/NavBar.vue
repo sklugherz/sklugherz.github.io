fb21853fd91e6fe6c530254601ad501a219371aa
sklugherz <sklugherz@gmail.com>
2025-03-25 17:22:10 -0700

    add blog assets folders

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,6 +7,7 @@ import { RouterLink } from 'vue-router';
     <nav>
         <RouterLink to="/"><h3>Stephen Klugherz</h3></RouterLink>
         <ul>
+            <li><RouterLink to="/blogs">blogs</RouterLink></li>
             <li><RouterLink to="/projects">projects</RouterLink></li>
             <li><RouterLink to="/now">now</RouterLink></li>
             <li><RouterLink to="/uses">uses</RouterLink></li>
