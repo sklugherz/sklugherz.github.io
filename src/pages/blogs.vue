def8fdc1d7649992d3eed72337120d9ea0fe432a
sklugherz <sklugherz@gmail.com>
2025-03-28 15:22:41 -0700

    blog desc

--- src/pages/blogs.vue
+++ src/pages/blogs.vue
@@ -7,13 +7,18 @@
         <li>
             <div class="link">
                 <RouterLink to="/blogs/phantom-intruder">
-                    <h3>Ph4nt0m 1ntrud3r challenge walkthrough from PicoCTF 2025</h3>
+                    <h3>Ph4nt0m 1ntrud3r CTF Challenge Walkthrough</h3>
                 </RouterLink>
             </div>
             <div class="desc">
-
+                <p>
+                    A walkthrough for a Capture the Flag challenge hosted on PicoCTF.
+                    This walkthrough covers packet analysis, packet filtering, and 
+                    data decoding to uncover the flag in a simulated SYN Flood attack.
+                </p>
             </div>
         </li>
+        
     </ul>
 
 </template>
@@ -32,6 +37,14 @@ ul {
 .link:hover {
     text-decoration: underline #f67634 dashed;
     transition: all 0.3s;
+}
+
+h3 {
+    padding-bottom: 5px;
+}
+
+li {
+    padding-bottom: 10px;
 }
 
 </style>
